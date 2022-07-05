068f20a1f5d41435d50a9b60fc439d557ecae70f
Jordan <jordan.b3360@gmail.com>
2022-07-05 14:38:55 -0500

    Password generator, refactored code to use arrays for criteria pool of password instead of strings

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cool-password-generator
-Password generator
+Password generator - a password generator that will take a users input and generate a random password
+
+# Description
+This project is a random password generator. When the generate button is pressed it will ask the user to choose a length between 8 and 128 characters long. Then it will ask if you would like to choose lower case, uppercase, numbers, and special characters as the password criteria. One must be selected in order to continue. Once one is selected in the confirm prompt a randomly generated password will appear in the password box on the page.
+
+# Screenshot
+
+![Screenshot of application](Screenshot1.png)
+
+# Link to Deployed App
+
+https://jordanb366.github.io/cool-password-generator/
--- script.js
+++ script.js
@@ -5,49 +5,52 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   // Ask user for password character length
 
-var password = "";
-var characterCriteriaPool = [""];
+  // Empty password variable
+var randomPassword = "";
 
-var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var randomLowerCase = Math.floor(Math.random()* lowerCaseArray.length);
-console.log(lowerCaseArray[randomLowerCase]);
+// Empty password variable
+var characterCriteriaPool = [];
 
-var upperCaseArray = ["A" , "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var randomUpperCase= Math.floor(Math.random()* upperCaseArray.length);
-console.log(upperCaseArray[randomUpperCase]);
+// Lowercase Array
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
+// upper Case array
+var upperCaseArray = ["A" , "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 
-var specialCharactersArray = ["!",  "@",  "#",  "$",  "%",  "^",  "&",  "*", "(",  ")"];
-var randomSpecialCharacter = Math.floor(Math.random()* specialCharactersArray.length);
-console.log(specialCharactersArray[randomSpecialCharacter]);
+// numbers array
+var numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9",];
 
-var numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var randomNumbers = Math.floor(Math.random()* numbersArray.length);
-console.log(numbersArray[randomNumbers]);
+// special characters array
+var specialCharactersArray = ["!",  "@",  "#",  "$",  "%",  "^",  "&",  "*", "(",  ")",];
+
 
 // Ask the user how many characters between 8 and 128 they would like the password to be
 var passwordLength = prompt("Password must be between 8 Characters and 128. How many characters?");
   if (passwordLength >= 8 && passwordLength <= 128)   {
     // Asks user if there should be upper case characters in the password
     var passwordLowerCase = confirm("Lowercase?");
+     // If lowercase is confirmed then store a variable called for the criteria selected
     if (passwordLowerCase == true) {
-      var lowerCase = "abcdefghijklmnopqrstuvwxyz";  
+      var lowerCase = lowerCaseArray; 
     } else {
       lowerCase = "";
     }
+   
     
     // Asks user if there should be upper case characters in the password
     var passwordUpperCase = confirm("Uppercase?");
+     // If uppercase is confirmed then store a variable called for the criteria selected
     if (passwordUpperCase == true) {
-      var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var upperCase = upperCaseArray;
     }  else {
       upperCase = "";
     }
     
     // Asks user if there should be numbers in the password
     var passwordNumbers = confirm("Numbers?");
+    // If numbers is confirmed then store a variable called for the criteria selected
     if (passwordNumbers == true) {
-      var numbers = "123456789";
+      var numbers = numbersArray;
     }  else {
       numbers = "";
     }
@@ -55,24 +58,26 @@ var passwordLength = prompt("Password must be between 8 Characters and 128. How 
     // Asks user if there should be special characters in the password
     var passwordSpecialCharacters = confirm("Special characters?");
     if (passwordSpecialCharacters == true) {
-      var specialCharacters = "!@#$%^&*()";
+    // If special characters is confirmed then store a variable called for the criteria selected
+      var specialCharacters = specialCharactersArray;
     }  else {
       specialCharacters = "";
     }
     
-  // Compare confirm data
+    // Compare confirm data
      if (passwordLowerCase === true || passwordUpperCase === true || passwordNumbers === true || passwordSpecialCharacters === true) {
-      //Store confirm criteria
-     characterCriteriaPool = lowerCase + upperCase + numbers + specialCharacters;
-      console.log(characterCriteriaPool);
+      // Add each criteria into one character pool by combining each selected
+     characterCriteriaPool = characterCriteriaPool.concat(lowerCase, upperCase, numbers, specialCharacters);
+  
             
- //Loop through password length
-  for (var i = 0; i <= passwordLength; i++) {
-   //generate the random password
-  var randomPassword = Math.floor(Math.random() * characterCriteriaPool.length);
- password += characterCriteriaPool.substring(randomPassword, randomPassword +1);
-}
-  return(password);      
+  //Loop through password length
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * characterCriteriaPool.length);
+    randomPassword = randomPassword + characterCriteriaPool[randomIndex];
+    
+  }
+  return(randomPassword);
+
   // If no criteria is selected an alert will pop up
       } else {
         alert("Your password criteria cannot be blank");
@@ -83,13 +88,6 @@ var passwordLength = prompt("Password must be between 8 Characters and 128. How 
     
   }
 };
-
-// for (var i = 0; i <= passwordLength; i++) {
-  // generate the random password
- // var randomPassword = Math.floor(Math.random() * characterCriteriaPool.length);
- // password += characterCriteriaPool.substring(randomPassword, randomPassword +1);
- //}
-
 
 // Write password to the #password input
 function writePassword() {
